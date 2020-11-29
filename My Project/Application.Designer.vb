a8d40a353ff150ef375a2f0bb2a9f45514584094
Matias Arratibel <matias.arratibel@gmail.com>
2020-11-29 15:57:18 -0300

    Forms añadidos y menu principal creado

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Manejo_bodegas.Form1
+            Me.MainForm = Global.Manejo_bodegas.FormMenuPrincipal
         End Sub
     End Class
 End Namespace
