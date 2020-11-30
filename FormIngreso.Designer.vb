4358dac3904e4be3d6197ef1be617d019c7e3b08
Matias Arratibel <matias.arratibel@gmail.com>
2020-11-29 21:33:17 -0300

    Finalizado el FormIngreso
    terminada la seccion de ingresar datos

--- FormIngreso.Designer.vb
+++ FormIngreso.Designer.vb
@@ -23,30 +23,201 @@ Partial Class FormIngreso
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btnVolver = New System.Windows.Forms.Button()
+        Me.txtCodigo = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtStock = New System.Windows.Forms.TextBox()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.comboProveedor = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnIngresarProducto = New System.Windows.Forms.Button()
+        Me.btnLimpiarCampos = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnVolver
         '
-        Me.btnVolver.Location = New System.Drawing.Point(560, 355)
+        Me.btnVolver.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnVolver.Location = New System.Drawing.Point(509, 275)
         Me.btnVolver.Name = "btnVolver"
-        Me.btnVolver.Size = New System.Drawing.Size(75, 23)
+        Me.btnVolver.Size = New System.Drawing.Size(126, 35)
         Me.btnVolver.TabIndex = 0
         Me.btnVolver.Text = "Volver"
         Me.btnVolver.UseVisualStyleBackColor = True
+        '
+        'txtCodigo
+        '
+        Me.txtCodigo.Location = New System.Drawing.Point(27, 96)
+        Me.txtCodigo.Name = "txtCodigo"
+        Me.txtCodigo.Size = New System.Drawing.Size(100, 20)
+        Me.txtCodigo.TabIndex = 1
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(141, 96)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 2
+        '
+        'txtStock
+        '
+        Me.txtStock.Location = New System.Drawing.Point(254, 96)
+        Me.txtStock.Name = "txtStock"
+        Me.txtStock.Size = New System.Drawing.Size(100, 20)
+        Me.txtStock.TabIndex = 3
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(366, 96)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecio.TabIndex = 4
+        '
+        'comboProveedor
+        '
+        Me.comboProveedor.FormattingEnabled = True
+        Me.comboProveedor.Location = New System.Drawing.Point(479, 95)
+        Me.comboProveedor.Name = "comboProveedor"
+        Me.comboProveedor.Size = New System.Drawing.Size(121, 21)
+        Me.comboProveedor.TabIndex = 5
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnLimpiarCampos)
+        Me.GroupBox1.Controls.Add(Me.btnIngresarProducto)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtPrecio)
+        Me.GroupBox1.Controls.Add(Me.comboProveedor)
+        Me.GroupBox1.Controls.Add(Me.txtCodigo)
+        Me.GroupBox1.Controls.Add(Me.txtNombre)
+        Me.GroupBox1.Controls.Add(Me.txtStock)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 10)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(623, 259)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingreso de productos"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(204, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(210, 25)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Ingresar un producto"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(24, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 15)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Codigo producto"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(138, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(103, 15)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Nombre producto"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(251, 78)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 15)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Stock"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(363, 78)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 15)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Precio unidad"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(476, 78)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(110, 15)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Nombre proveedor"
+        '
+        'btnIngresarProducto
+        '
+        Me.btnIngresarProducto.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIngresarProducto.Location = New System.Drawing.Point(209, 142)
+        Me.btnIngresarProducto.Name = "btnIngresarProducto"
+        Me.btnIngresarProducto.Size = New System.Drawing.Size(205, 41)
+        Me.btnIngresarProducto.TabIndex = 12
+        Me.btnIngresarProducto.Text = "Ingresar producto"
+        Me.btnIngresarProducto.UseVisualStyleBackColor = True
+        '
+        'btnLimpiarCampos
+        '
+        Me.btnLimpiarCampos.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLimpiarCampos.Location = New System.Drawing.Point(209, 199)
+        Me.btnLimpiarCampos.Name = "btnLimpiarCampos"
+        Me.btnLimpiarCampos.Size = New System.Drawing.Size(205, 41)
+        Me.btnLimpiarCampos.TabIndex = 13
+        Me.btnLimpiarCampos.Text = "Limpiar campos"
+        Me.btnLimpiarCampos.UseVisualStyleBackColor = True
         '
         'FormIngreso
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(647, 390)
+        Me.ClientSize = New System.Drawing.Size(647, 318)
         Me.ControlBox = False
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.btnVolver)
         Me.Name = "FormIngreso"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Ingreso de productos a la bodega"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents btnVolver As Button
+    Friend WithEvents txtCodigo As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtStock As TextBox
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents comboProveedor As ComboBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnLimpiarCampos As Button
+    Friend WithEvents btnIngresarProducto As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
 End Class
--- FormIngreso.vb
+++ FormIngreso.vb
@@ -3,6 +3,8 @@
 Public Class FormIngreso
 
     Dim frmMenuPrincipal As FormMenuPrincipal
+    Dim proveedores As New Dictionary(Of Integer, String)
+    Dim conexion As Conexion
 
     Public Sub New(menuForm As FormMenuPrincipal)
 
@@ -10,10 +12,79 @@ Public Class FormIngreso
 
         Me.frmMenuPrincipal = menuForm
 
+        conexion = New Conexion("127.0.0.1", "root", "", "dbbodega")
+
+        proveedores = conexion.getProveedores()
+
+        For Each kvp As KeyValuePair(Of Integer, String) In proveedores
+
+            comboProveedor.Items.Add(kvp.Value)
+
+        Next
+
+        comboProveedor.SelectedIndex = 0
+
     End Sub
 
+    Private Sub btnIngresarProducto_Click(sender As Object, e As EventArgs) Handles btnIngresarProducto.Click
+
+        Try
+
+            Dim idProducto, stock, precio, idProveedor As Integer
+            Dim nombreProducto As String
+
+            If txtCodigo.Text.Equals("") Or txtNombre.Text.Equals("") Or txtStock.Text.Equals("") Or txtPrecio.Text.Equals("") Then
+
+                MsgBox("Debes rellenar todos los campos correctamente")
+
+            Else
+
+                Try
+
+                    idProducto = Convert.ToInt32(txtCodigo.Text)
+                    stock = Convert.ToInt32(txtStock.Text)
+                    precio = Convert.ToInt32(txtPrecio.Text)
+                    nombreProducto = txtNombre.Text
+
+                    For Each kvp As KeyValuePair(Of Integer, String) In proveedores
+
+                        If kvp.Value.Equals(comboProveedor.SelectedIndex) Then
+
+                            idProveedor = kvp.Key
+
+                        End If
+
+                    Next
+
+                    Dim comando = String.Format("INSERT INTO productos(idProductos, nombreProducto, Stock, Precio, Proveedor_idProveedor) VALUES({0}, '{1}', {2}, {3}, {4})", idProducto, nombreProducto, stock, precio, idProveedor)
+
+                    conexion.enviarComando(comando)
+
+                Catch ex As Exception
+
+                    MsgBox("No puede haber texto en los campos numericos (Codigo Producto, stock, precio unidad)")
+
+                End Try
+
+            End If
 
 
+        Catch ex As Exception
+
+            MsgBox("AH OCURRIDO UN ERROR " & ex.ToString())
+
+        End Try
+
+    End Sub
+
+    Private Sub btnLimpiarCampos_Click(sender As Object, e As EventArgs) Handles btnLimpiarCampos.Click
+
+        txtCodigo.Clear()
+        txtNombre.Clear()
+        txtPrecio.Clear()
+        txtStock.Clear()
+
+    End Sub
 
     Private Sub btnVolver_Click(sender As Object, e As EventArgs) Handles btnVolver.Click
 
